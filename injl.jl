bdbf0223508a44aba0fdcc4c8a675c917e58a4ff
anandijain <anandj@uchicago.edu>
2019-10-10 16:40:14 -0500

    able to train, todo batching, plotting next

---
+++ injl.jl
@@ -0,0 +1,84 @@
+using CSV, DataFrames
+using VideoIO, Images #, Makie
+using Plots
+using Flux
+using Flux: @epochs
+
+FRAME_LEN = 512
+TRAIN_FN = "./data/train.mp4"
+TEST_FN = "./data/test.mp4"
+LABELS_FN = "./data/train.txt"
+
+
+function get_speeds()
+	df = CSV.read(LABELS_FN, header=["mph"])
+	speeds = convert(Vector, df.mph)
+	return speeds
+end
+
+
+function main()
+	io = VideoIO.open(TRAIN_FN)
+	vid = VideoIO.openvideo(io)
+	return io, vid
+end
+
+
+function run()
+	io, vid = main()
+	speeds = get_speeds()
+	img = read(vid)
+	scene = Makie.Scene(resolution = size(img))
+	makieimg = Makie.image!(scene, img)[end]
+	Makie.rotate!(scene, -0.5pi)
+	display(scene)
+	i = 1
+	while !eof(vid)
+		println(speeds[i])
+    		read!(vid, img)
+    		makieimg[1] = img
+    		sleep(1/vid.framerate)
+		i += 1
+	end
+end
+
+io, vid = main()
+speeds = get_speeds()
+
+m = Chain(Dense(640*480, 1000, tanh), Dense(1000, 1))
+
+function loss(x, y)
+	img = view(Gray.(x), :)
+	l = Flux.mse(m(img), y)
+	return l
+end
+
+function make_tups(vid, speeds)
+	i = 1
+	tups = []
+	while !eof(vid)
+		img = read(vid)
+		speed = speeds[i]
+		push!(tups, (img, speed))
+		i += 1
+	end
+	return tups
+end
+tups = make_tups(vid, speeds)
+
+ps = Flux.params(m)
+loss(tups[1]...)
+
+opt = ADAM()
+num_tups = length(tups)
+
+function evalcb()
+	test_x, test_y = tups[rand(1:num_tups)]
+	img = view(Gray.(test_x), :)
+	plot(test_x)
+	@show(loss(test_x, test_y))
+	@show(m(img), test_y)
+end
+
+Flux.train!(loss, ps, tups, opt, cb = Flux.throttle(evalcb, 5))
+# run()
---
+++ run.py
@@ -0,0 +1,47 @@
+import os
+import time
+import cv2
+import numpy as np
+
+import torch
+import torchvision as tv
+
+# import matplotlib.pyplot as plt
+import pandas as pd
+
+FRAME_LEN = 512
+TRAIN_FN = './data/train.mp4'
+TEST_FN = './data/test.mp4'
+LABELS_FN = './data/train.txt'
+
+def read_video(start_frame, stop_frame):
+  vid = tv.io.read_video(TRAIN_FN, start_pts=FRAME_LEN*start_frame, end_pts=FRAME_LEN*stop_frame)
+  return vid
+
+def frame_speeds():
+  df = pd.read_csv(LABELS_FN, sep='\n', header=None)
+  df.columns = ['mph']
+  return df
+
+def cv2read():
+  speeds = frame_speeds().values
+  cap = cv2.VideoCapture(TRAIN_FN)
+  i = 0
+  while(cap.isOpened()):
+    ret, frame = cap.read()
+    speed = str(speeds[i][0])
+    print(speed)
+    cv2.imshow('f', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    time.sleep(0.033)
+    i += 1
+  cap.release()
+  cv2.destroyAllWindows()
+
+
+
+if __name__ == '__main__':
+  # vid = read_video()
+  cv2read()
